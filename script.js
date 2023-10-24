14d120734d67120badf15e6cd7c3c25512960461
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2023-10-24 09:38:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-document.getElementById('calcForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    // Initial graph draw based on default values
+    calculateAndDisplay();
 
+    // Attach event listener to the form's submission (Calculate button click)
+    document.getElementById('calcForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        calculateAndDisplay();
+    });
+});
+
+function calculateAndDisplay() {
     let pricePerCheckout = parseFloat(document.getElementById('pricePerCheckout').value);
     let numPatrons = parseFloat(document.getElementById('numPatrons').value);
     let avgTitles = parseFloat(document.getElementById('avgTitles').value);
@@ -10,11 +19,17 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
     let profit = revenue - cost;
 
     displayChart(revenue, cost, profit);
-});
+}
 
 function displayChart(revenue, cost, profit) {
     var ctx = document.getElementById('profitChart').getContext('2d');
-    var chart = new Chart(ctx, {
+
+    // If a chart already exists, destroy it to avoid overlapping data
+    if (window.chartInstance) {
+        window.chartInstance.destroy();
+    }
+
+    window.chartInstance = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['Revenue', 'Cost', 'Profit'],
